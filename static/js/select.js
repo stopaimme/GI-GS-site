62c6f2d925e51ddb6c7bcc6062c2daecef6da91e
stopaimme <56530653+stopaimme@users.noreply.github.com>
2024-10-04 07:02:43 +0800

    Update select.js

--- static/js/select.js
+++ static/js/select.js
@@ -18,12 +18,7 @@ function ChangeScene(idx){
     }
     li_list[idx].className = "active";
     currentScene = currentSceneList[idx]
-    document.getElementById("render").src = "static/videos/" + currentScene +'/'+ "render" +'/'+'.mp4';
-    document.getElementById("occlusion").src = "static/videos/" + currentScene +'/'+ "occlusion" +'/'+'.mp4';
-    document.getElementById("indirect").src = "static/videos/" + currentScene +'/'+ "indirect" +'/'+'.mp4';
-    document.getElementById("normal").src = "static/videos/" + currentScene +'/'+ "normal" +'/'+'.mp4';
-    document.getElementById("albedo").src = "static/videos/" + currentScene +'/'+ "albedo" +'/'+'.mp4';
-    document.getElementById("roughness").src = "static/videos/" + currentScene +'/'+ "roughness" +'/'+'.mp4';
+    document.getElementById("all_video").src = "static/videos/" + currentScene +'.mp4';
 }
 
 function ChangeObject(idx){
@@ -38,10 +33,5 @@ function ChangeObject(idx){
     }
     li_list[idx].className = "active";
     currentObject = currentObjectList[idx]
-    document.getElementById("render").src = "static/videos/" + currentObject +'/'+ "render" +'/'+'.mp4';
-    document.getElementById("occlusion").src = "static/videos/" + currentObject +'/'+ "occlusion" +'/'+'.mp4';
-    document.getElementById("indirect").src = "static/videos/" + currentObject +'/'+ "indirect" +'/'+'.mp4';
-    document.getElementById("normal").src = "static/videos/" + currentObject +'/'+ "normal" +'/'+'.mp4';
-    document.getElementById("albedo").src = "static/videos/" + currentObject +'/'+ "albedo" +'/'+'.mp4';
-    document.getElementById("roughness").src = "static/videos/" + currentObject +'/'+ "roughness" +'/'+'.mp4';
+    document.getElementById("all_video").src = "static/videos/" + currentScene +'.mp4';
 }
