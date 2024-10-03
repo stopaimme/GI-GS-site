16af62b69f4580e97f222e437a15908d4709bf12
stopaimme <56530653+stopaimme@users.noreply.github.com>
2024-10-04 06:28:21 +0800

    Update select.js

--- static/js/select.js
+++ static/js/select.js
@@ -1,6 +1,9 @@
 var currentSceneList = ["bicycle", "garden","stump", "lego", "hotdog", "armadillo", "ficus"];
 var currentScene = "bicycle";
 
+var currentSceneList = ["lego", "hotdog", "armadillo", "ficus"];
+var currentScene = "lego";
+
 
 
 function ChangeScene(idx){
@@ -21,4 +24,24 @@ function ChangeScene(idx){
     document.getElementById("normal").src = "static/videos/" + currentScene +'/'+ "normal" +'/'+'.mp4';
     document.getElementById("albedo").src = "static/videos/" + currentScene +'/'+ "albedo" +'/'+'.mp4';
     document.getElementById("roughness").src = "static/videos/" + currentScene +'/'+ "roughness" +'/'+'.mp4';
+}
+
+function ChangeObject(idx){
+    var li_list = document.getElementById("object").children;
+    console.log(idx);
+    console.log(li_list);
+    for(i = 0; i < li_list.length; i++){
+        if (li_list[i].className === "disabled"){
+            continue
+        }
+        li_list[i].className = "";
+    }
+    li_list[idx].className = "active";
+    currentObject = currentObjectList[idx]
+    document.getElementById("render").src = "static/videos/" + currentObject +'/'+ "render" +'/'+'.mp4';
+    document.getElementById("occlusion").src = "static/videos/" + currentObject +'/'+ "occlusion" +'/'+'.mp4';
+    document.getElementById("indirect").src = "static/videos/" + currentObject +'/'+ "indirect" +'/'+'.mp4';
+    document.getElementById("normal").src = "static/videos/" + currentObject +'/'+ "normal" +'/'+'.mp4';
+    document.getElementById("albedo").src = "static/videos/" + currentObject +'/'+ "albedo" +'/'+'.mp4';
+    document.getElementById("roughness").src = "static/videos/" + currentObject +'/'+ "roughness" +'/'+'.mp4';
 }
